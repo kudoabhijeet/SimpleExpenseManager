14679363e612ba5c48c0b15f985f70ac45cac61c
Abhijeet Prasad <abhi.prasad16@mgmail.com>
2019-01-16 13:03:43 +0530

    Added Debit functions

--- ExpenseManager.py
+++ ExpenseManager.py
@@ -27,7 +27,16 @@ def credit() :
     else :
         print 'Invalid Input!!'
 
-#Debit Function
+#Debit Functions
+def add_debit() :
+    c1 = open("debit_file.txt", "a")
+    value = input()
+    c1.write('%d \n' %value)
+    c1.close()
+def view_debit() :
+    v1 =  open("debit_file.txt", "r")
+    for money in v1 :
+        print money
 def debit() :
     print 'Choose an option :'
     print '1. Add to a new file'
@@ -35,16 +44,16 @@ def debit() :
     print '3. Exit'
     debit_input = input()
     if (debit_input == 1) :
-        print 'Open a new file here'
+        add_debit()
     elif (debit_input == 2) :
-        print 'Open a previous file with append mode'
+        view_debit()
     elif (debit_input == 3) :
         sys.exit()
     else :
         print 'Invalid Input!!'
 
 #Modify Function
-def modify() :
+def modify_c() :
     print 'Choose an option'
     print '1. View Previous Records'
     print '2. Delete a record file'
@@ -54,11 +63,27 @@ def modify() :
     if (modify_input == 1) :
         view_credit()
     elif (modify_input == 2) :
-        print 'Delete a file'
+        print 'Deleted a file'
     elif (modify_input == 3) :
         sys.exit()
     else :
         print 'Invalid Input!!'
+
+def modify_d() :
+    print 'Choose an option'
+    print '1. View Previous Records'
+    print '2. Delete a record file'
+    print '3. Exit'
+
+    modify_input =  input()
+    if (modify_input == 1) :
+        view_debit()
+    elif (modify_input == 2) :
+        print 'Deleted a file'
+    elif (modify_input == 3) :
+        sys.exit()
+    else :
+        print 'Invalid Input!!
 
 #Main Function Starts Here!!...
 
@@ -66,8 +91,9 @@ print 'Welcome to Expense Records Manager'
 print 'Choose an option :'
 print '1. Credit'
 print '2. Debit'
-print '3. View/Modify prev. records'
-print '4. Exit application'
+print '3. View/Modify prev. Credit records'
+print '4. View/Modify prev. Debit records'
+print '5. Exit application'
 
 user_input = input()
 print 'Option Selected = %d' %user_input
@@ -79,9 +105,11 @@ elif (user_input == 2) :
     debit()
     #print 'Debit Function called'
 elif (user_input == 3) :
-    modify()
+    modify_c()
     #print 'Modify Function called'
 elif (user_input == 4) :
+    modify_d()
+elif (user_input == 5) :
     sys.exit()
 else :
     print 'Invalid Input!!'
